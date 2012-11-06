1c58b4cf52300405dae76b176b8e13fe4d714bc5
Jan Carlo Viray <virayjancarlo@yahoo.com>
2012-11-06 00:32:45 -0800

    code cleanup and organization

--- app.js
+++ app.js
@@ -1,69 +1,73 @@
-/*
-TODO
-- cleanup and organize
-- make into modules
-- make this a usable library -> upload to github
-- makeBarGraph(data,options)
-- options: title, legend, fontsize, fontcolor, background, gridcolor
-*/
+//OPTIONS:
+//barGap 5, barWidth 20, barPosX 5
+//backgroundColor, fontColor, fontSettings, gridColor
 
-var app = (function(){
+document.addEventListener('DOMContentLoaded',BarChart);
 
-	var canvas = document.getElementById('canvas');
-	var ctx = canvas.getContext('2d');
-
-	var data = [60,40,78,59,84,95,63,42,31,53,56,53,43,13,53,64,13];
-
-	var canvasHeight = canvas.height, canvasWidth = canvas.width,
-	  barGap = 5, barWidth = 20, barPosX = 5;
-
-	function init(){
+var BarChart = (function() {
+	
+	console.log('loaded');
+	var canvas, ctx, data, options,
+		canvasHeight, canvasWidth, barGap, barWidth, barPosX;
+	
+	function init( canvasId, userData, userOptions){
+		canvas = document.getElementById( canvasId );
+		ctx = canvas.getContext('2d');
+		
+		data = userData || [];
+		options = userOptions || {};
+		
+		canvasHeight = canvas.height;
+		canvasWidth = canvas.width;
+		barGap = options.barGap || 5;
+		barWidth = options.barWidth || 20;
+		barPosX = options.barPosX || 5;
+		
 		autoResizeToCanvas();
 		prepareCanvas();
 		drawGrid();
 		drawGraph();
 	}
 
-	function prepareCanvas(){
-		//rect(x,y,width,height)
-		ctx.fillStyle = 'rgb(30,30,30)';
-		ctx.fillRect(0,0,400,200);
+	function prepareCanvas() {
+		ctx.fillStyle = options.backgroundColor || 'rgb(30,30,30)';
+		ctx.fillRect( 0, 0, canvasWidth, canvasHeight );
 	}
 
 	function autoResizeToCanvas(){
 		//why canvasWidth - 5? --> to fix last bar touching edge
 		//why - barGap? --> to fix first bar touching edge
-		barWidth = ((canvasWidth-5) / data.length) - barGap;
+		barWidth = ( ( canvasWidth - barGap ) / data.length ) - barGap;
 	}
 
-	function drawGraph(){
+	function drawGraph() {
 		for( var i = 0; i < data.length; i++ ){
 		  
 			//change color based on previous data
-			if(data[i] < data[i-1]) ctx.fillStyle = 'rgba(250,100,100,.7)';
+			if( data[i] < data[i-1] ) ctx.fillStyle = 'rgba(250,100,100,.7)';
 			else ctx.fillStyle = 'rgba(50,250,200,.8)';
 
 			//ctx.fillRect(barPosX,0,barWidth,data[i]);
 			//why change? since origin is top-left; see -data[i] and canvasHeight
-			ctx.fillRect(barPosX,canvasHeight,barWidth,-data[i]); 
+			ctx.fillRect( barPosX, canvasHeight, barWidth, -data[i] ); 
 
-			ctx.fillStyle = 'white';
-			ctx.font = 'bold 13px Arial';
+			ctx.fillStyle = options.fontColor || 'white';
+			ctx.font = options.fontSettings || 'bold 13px Arial';
 			ctx.fillText( data[i], barPosX, canvasHeight - data[i] - 7 );
 
 			barPosX += barWidth + barGap;
 		}
 	}
 
-	function drawGrid(){
+	function drawGrid() {
 		var gridGap = canvasHeight / 10, currentGridY = 0;
 		ctx.lineWidth = 2;
-		ctx.strokeStyle = 'rgb(50,50,50)';
+		ctx.strokeStyle = options.gridColor || 'rgb(50,50,50)';
 		
 		for( var i = 1; i <= 100; i++ ) {
 			ctx.beginPath();
-			ctx.moveTo(0,currentGridY);
-			ctx.lineTo(canvasWidth,currentGridY);
+			ctx.moveTo( 0, currentGridY );
+			ctx.lineTo( canvasWidth, currentGridY );
 			ctx.closePath();
 			ctx.stroke();
 			currentGridY += gridGap;
@@ -71,10 +75,7 @@ var app = (function(){
 	}
   
   return {
-    init : init
+    render : init
   };
-  
-    
 })();
 
-app.init();
